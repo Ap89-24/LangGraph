9d286bd0ffcf92623b0aa557be443cf059ae7c4a
Ap89-24 <aman082199@gmail.com>
2026-09-04 00:44:13 +0530

    feat(realtime): display live Kubernetes events via WebSocket

--- chatbotInterface/chatbot_backend.py
+++ chatbotInterface/chatbot_backend.py
@@ -39,18 +39,18 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
-chatbot
 
 
 
-thread_id = '1'
 
-while True:
+# thread_id = '1'
+
+# while True:
     
-    user_message = input('Type here: ')
-    print('User: ' , user_message)
-    if(user_message.strip().lower() in ['exit','quit','bye']):
-        break
-    config = {'configurable': {'thread_id': thread_id}}
-    response = chatbot.invoke({'messages': HumanMessage(content=user_message)}, config=config)
-    print('AI: ' ,response['messages'][-1].content)    
+#     user_message = input('Type here: ')
+#     print('User: ' , user_message)
+#     if(user_message.strip().lower() in ['exit','quit','bye']):
+#         break
+#     config = {'configurable': {'thread_id': thread_id}}
+#     response = chatbot.invoke({'messages': HumanMessage(content=user_message)}, config=config)
+#     print('AI: ' ,response['messages'][-1].content)    
--- chatbotInterface/chatbot_frontend.py
+++ chatbotInterface/chatbot_frontend.py
@@ -1,10 +1,17 @@
 import streamlit as st 
 from chatbot_backend import chatbot
 from langchain_core.messages import HumanMessage
+import time
   
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}    
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []   
+
+
+def stream_with_delay(chunks, delay=0.02):
+    for chunk in chunks:
+        yield chunk
+        time.sleep(delay) 
 
 for message in st.session_state['message_history']: 
     with st.chat_message(message['role']):
@@ -17,8 +24,16 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
      
-    response = chatbot.invoke({'messages': HumanMessage(content=user_input)} , config=CONFIG)
-    ai_response = response['messages'][-1].content
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_response})   
+    
+    
+     
     with st.chat_message("assistant"):
-        st.text(ai_response)    
+        ai_response = st.write_stream(
+            stream_with_delay((message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config = {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode='messages'
+            )), delay=0.03)
+        )   
+        
+        st.session_state['message_history'].append({'role': 'assistant', 'content': ai_response})     
