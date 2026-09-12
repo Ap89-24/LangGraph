3b8541f082a2a97152d10a9cadecd565cd3c99ca
Ap89-24 <aman082199@gmail.com>
2026-09-12 22:16:38 +0530

    add langsmith observability

--- chatbotInterface/chatbot_frontend.py
+++ chatbotInterface/chatbot_frontend.py
@@ -101,7 +101,13 @@ if user_input:
         st.markdown(user_input)
      
     
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}} 
+    CONFIG = {
+        'configurable': {'thread_id': st.session_state['thread_id']},
+        'metadata': {
+              'thread_id': st.session_state['thread_id']
+            },
+        'run_name': 'chat_turn' 
+        } 
     
      
     with st.chat_message("assistant"):
